c72e04707c061107c2f7083bb605c223b359f9be
Pazuzu Jindrich <Pazuzujindrich@gmail.com>
2019-06-11 20:19:16 -0700

    Delete NotePadApp.java

--- NotePadApp.java
+++
@@ -1,9 +0,0 @@
-package HW_03;
-
-public class NotePadApp {
-    public static void main(String[] args) {
-    	NotePadModel model = new NotePadModel();
-        NotePadView view = new NotePadView();
-    	NotePadController controller = new NotePadController(view, model);
-    }
-}
